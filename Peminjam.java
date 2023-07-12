17cdf0ce5abe780d81ba9386d8ed283f933f6554
IbrahimWicaksono2002 <wicaksonoibrahim334@gmail.com>
2023-07-12 17:16:34 +0700

    UAS_PBO_G.111.20.0004

--- Peminjam.java
+++ Peminjam.java
@@ -1,6 +1,6 @@
 public abstract class Peminjam implements UserLogin {
-
     private boolean isLogin;
+    private boolean isAntiDenda;
 
     @Override
     public void doLogin() {
@@ -10,18 +10,30 @@ public abstract class Peminjam implements UserLogin {
 
     @Override
     public void doLogout() {
-         System.out.println("Peminjam berhasil logout");
+        System.out.println("Peminjam berhasil logout");
         isLogin = false;
     }
-    
+
     @Override
     public boolean isLogin() {
         return isLogin;
     }
 
-
     public abstract void setKodePeminjam(String kodePeminjam);
+
     public abstract String getKodePeminjam();
+
     public abstract void setNamaPeminjam(String namaPeminjam);
+
     public abstract String getNamaPeminjam();
+    
+    public void addAntiDenda() {
+        System.out.println("Tambah Denda");
+        isAntiDenda = true;
+    }
+
+    public void removeAntiDenda() {
+        System.out.println("Hapus Denda");
+        isAntiDenda = true;
+}
 }
